15137a3730e5b3075509df892436c05f82d23d6a
Dhanashree2023 <Prachi123451@gmail.com>
2024-02-03 08:13:15 +0530

    Test 1 & 2

--- src/oct2023com/Test.java
+++ src/oct2023com/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hellow World");
+		
+		System.out.println("Test case 1");
+		
+		System.out.println("Test case 2");
 
 	}
 
